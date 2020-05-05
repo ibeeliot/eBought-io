0cc3661c3cc98fbe8195909af06d1c0aa1603a3a
ibeeliot <eliotlefrin@gmail.com>
2020-05-04 23:15:16 -0700

    fixed res.profile to req.profile

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const { createCategory } = require('../controllers/categoryControllers.js');
 const { requiredLogin, isAuth, isAdmin } = require('../controllers/userAuth');
 
-router.post('/category/create', createCategory);
+router.post(
+	'/category/create/:userId',
+	requiredLogin,
+	isAuth,
+	isAdmin,
+	createCategory
+);
 
 module.exports = router;
